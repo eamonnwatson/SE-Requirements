3d7f46648421002b71034e4473bb8a4d6ddeaf38
Eamonn Watson <Eamonn.Watson@iqor.com>
2020-02-26 23:07:12 -0400

    Initial Projects Initial Parser

---
+++ src/SEConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using SELibrary;
+using System;
+
+namespace SEConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+
+            parser.GetData();
+        }
+    }
+}
---
+++ src/SELibrary/Parser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Linq;
+
+namespace SELibrary
+{
+    public class Parser
+    {
+        public void GetData()
+        {
+            var displayNames = GetDisplayNames();
+            var doc = new XmlDocument();
+            doc.Load("..//..//..//..//..//data//BlueprintClasses.sbc");
+
+
+        }
+
+        private Dictionary<string, string> GetDisplayNames()
+        {
+            var output = new Dictionary<string, string>();
+            var doc = new XmlDocument();
+            doc.Load("..//..//..//..//..//data//Localization//MyTexts.resx");
+            var nodes = doc.GetElementsByTagName("data");
+            foreach (XmlNode item in nodes)
+            {
+                var key = item.Attributes["name"].Value;
+                var value = item.ChildNodes.Cast<XmlNode>().Where(n => n.Name.Equals("value")).FirstOrDefault()?.InnerText;
+
+                output.Add(key, value);
+            }
+
+            return output;
+        }
+    }
+}
